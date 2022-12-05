f0e243c1d9c0afbc702eb3f9e3c212d9d6de9c17
Francisco González Ibáñez <fragonib@gmail.com>
2022-12-05 11:54:04 +0100

    day2

---
+++ lib/day2/day21.ex
@@ -0,0 +1,66 @@
+defmodule AoC.Day21 do
+
+  @spec mano(String) :: :paper | :rock | :scissors
+  def mano(code) do
+    case code do
+      "A" -> :rock
+      "B" -> :paper
+      "C" -> :scissors
+      "X" -> :rock
+      "Y" -> :paper
+      "Z" -> :scissors
+      _ -> :none
+    end
+  end
+
+  def parsePlay(n) do
+    String.split(n)
+    |> Enum.map(&mano/1)
+  end
+
+  def readProblem do
+    IO.stream(:stdio, :line)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&(parsePlay/1))
+    |> Enum.to_list()
+  end
+
+  @spec winner(nonempty_maybe_improper_list) :: boolean
+  def winner([player1 | player2]) do
+    case player1 do
+       :rock -> player2 == :paper
+       :paper -> player2 == :scissors
+       :scissors -> player2 == :rock
+    end
+  end
+
+  def outcome([player1 | player2]) when player1 == player2, do: 3
+  def outcome(play) do
+    case winner(play) do
+      false -> 3
+      true -> 6
+    end
+  end
+
+  def shape([_ | [player2]]) do
+    case player2 do
+       :rock -> 1
+       :paper -> 2
+       :scissors -> 3
+    end
+  end
+
+  def resolve(problem) do
+    problem
+    |> Stream.map(fn (x) -> outcome(x) + shape(x) end)
+    |> Enum.sum()
+  end
+
+  def printResults() do
+    readProblem()
+    |> resolve()
+    |> Integer.to_string()
+    |> IO.puts()
+  end
+
+end
---
+++ test/day2/day21.ex
@@ -0,0 +1,22 @@
+defmodule AoC.Day21Test do
+  use ExUnit.Case
+  doctest AoC.Day21
+  alias AoC.Day21
+
+  test "read" do
+    assert Day21.readProblem() == [
+      [:rock, :paper],
+      [:paper, :rock],
+      [:scissors, :scissors]
+    ]
+  end
+
+  test "resolve" do
+    assert Day21.resolve([
+      [:rock, :paper],
+      [:paper, :rock],
+      [:scissors, :scissors]
+    ]) == 15
+  end
+
+end
