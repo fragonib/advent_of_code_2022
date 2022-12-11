6fa24256529680bcbcd064e73fc547a7a462da7b
Francisco González Ibáñez <fragonib@gmail.com>
2022-12-11 16:48:08 +0100

    day4: implement right expected behavi

--- lib/day4/day41.ex
+++ lib/day4/day41.ex
@@ -5,12 +5,12 @@ defmodule AoC.Day41 do
     |> Stream.map(&String.trim/1)
     |> Stream.map(&readPair/1)
     |> Enum.to_list()
-    |> List.flatten()
   end
 
   def readPair(line) do
     String.split(line, ",", trim: true)
     |> Enum.map(&readRange/1)
+    |> List.to_tuple()
   end
 
   def readRange(pairLiteral) do
@@ -21,28 +21,13 @@ defmodule AoC.Day41 do
 
   def resolve(problem) do
     problem
-    |> evaluate_ranges_overlap()
-    # |> Enum.map(&boolean_to_integer/1)
-    # |> Enum.sum()
+    |> Enum.map(&ranges_overlap?/1)
+    |> Enum.map(&boolean_to_integer/1)
+    |> Enum.sum()
   end
 
-  def evaluate_ranges_overlap(all) do
-    case all do
-      [] -> []
-      [_] -> [false]
-      [target | others] -> [has_overlapping(target, others)] ++ evaluate_ranges_overlap(others)
-    end
-  end
 
-  def has_overlapping(range, other_ranges) do
-    case other_ranges do
-      [] -> false
-      [other_range] -> ranges_overlap?(range, other_range)
-      [other_range | others] -> ranges_overlap?(range, other_range) || has_overlapping(range, others)
-    end
-  end
-
-  def ranges_overlap?({r1_lower, r1_upper}, {r2_lower, r2_upper}) do
+  def ranges_overlap?({{r1_lower, r1_upper}, {r2_lower, r2_upper}}) do
     r1_lower >= r2_lower && r1_upper <= r2_upper ||
     r2_lower >= r1_lower && r2_upper <= r1_upper
   end
--- test/day4/day41.ex
+++ test/day4/day41.ex
@@ -5,43 +5,31 @@ defmodule AoC.Day41Test do
 
   test "read problem" do
     assert Day41.readProblem() == [
-      { 2, 4 },
-      { 6, 8 },
-      { 2, 3 },
-      { 4, 5 },
-      { 5, 7 },
-      { 7, 9 },
-      { 2, 8 },
-      { 3, 7 },
-      { 6, 6 },
-      { 4, 6 },
-      { 2, 6 },
-      { 4, 8 },
+      {{ 2, 4 }, { 6, 8 }},
+      {{ 2, 3 }, { 4, 5 }},
+      {{ 5, 7 }, { 7, 9 }},
+      {{ 2, 8 }, { 3, 7 }},
+      {{ 6, 6 }, { 4, 6 }},
+      {{ 2, 6 }, { 4, 8 }},
     ]
   end
 
   test "range overlap" do
-    assert Day41.ranges_overlap?({2,8}, {3,7}) == true
-    assert Day41.ranges_overlap?({2,8}, {3,9}) == false
-    assert Day41.ranges_overlap?({3,7}, {2,8}) == true
-    assert Day41.ranges_overlap?({6,6}, {4,6}) == true
-    assert Day41.ranges_overlap?({6,6}, {4,5}) == false
+    assert Day41.ranges_overlap?({{2,8}, {3,7}}) == true
+    assert Day41.ranges_overlap?({{2,8}, {3,9}}) == false
+    assert Day41.ranges_overlap?({{3,7}, {2,8}}) == true
+    assert Day41.ranges_overlap?({{6,6}, {4,6}}) == true
+    assert Day41.ranges_overlap?({{6,6}, {4,5}}) == false
   end
 
   test "resolve" do
     assert Day41.resolve([
-      { 2, 4 },
-      { 6, 8 },
-      { 2, 3 },
-      { 4, 5 },
-      { 5, 7 },
-      { 7, 9 },
-      { 2, 8 },
-      { 3, 7 },
-      { 6, 6 },
-      { 4, 6 },
-      { 2, 6 },
-      { 4, 8 },
+      {{ 2, 4 }, { 6, 8 }},
+      {{ 2, 3 }, { 4, 5 }},
+      {{ 5, 7 }, { 7, 9 }},
+      {{ 2, 8 }, { 3, 7 }},
+      {{ 6, 6 }, { 4, 6 }},
+      {{ 2, 6 }, { 4, 8 }},
     ]) == 2
   end
 
