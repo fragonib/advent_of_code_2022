def9c72e8485d3de68aebb168ad9bf55138e3e64
Francisco González Ibáñez <fragonib@gmail.com>
2022-12-13 18:52:34 +0100

    day5: skeleton

--- test/day5/day51.ex
+++ test/day5/day51.ex
@@ -4,9 +4,19 @@ defmodule AoC.Day51Test do
   alias AoC.Day51
 
   test "read problem" do
-    assert Day51.readProblem() == [
-      "[D]", "[N] [C]", "[Z] [M] [P]", "1   2   3", "", "move 1 from 2 to 1", "move 3 from 1 to 3", "move 2 from 2 to 1", "move 1 from 1 to 2"
-    ]
+    assert Day51.readProblem() == %{
+      :stacks => [
+        %{ :id => 1, :stack => ["D"] },
+        %{ :id => 2, :stack => ["N", "C"] },
+        %{ :id => 3, :stack => ["Z", "M", "P"] }
+      ],
+      :movements => [
+        %{ :quantity => 1, :from => 2, :to => 1 },
+        %{ :quantity => 3, :from => 1, :to => 3 },
+        %{ :quantity => 2, :from => 2, :to => 1 },
+        %{ :quantity => 1, :from => 1, :to => 2 }
+      ]
+    }
   end
 
 end
